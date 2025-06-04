ca1e59373bd05a91e968eb6a30df6f924bb46a4d
Andre-gusto <Andrelopes4808@gmail.com>
2025-06-03 22:27:14 -0300

    CRUD

--- pagina_eventos/crud/confirm.php
+++ pagina_eventos/crud/confirm.php
@@ -1,16 +1,5 @@
 <?php
-// Conexão com o banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nome_do_banco"; // 🔁 Substitua pelo nome real do seu banco
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
+include 'db.php';
 
 // Recebe os dados do formulário
 $id = $_POST['evento_id'];
@@ -18,18 +7,37 @@ $colaborador = $_POST['evento_idColaborador'];
 $responsavel = $_POST['evento_idResponsavel'];
 $categoria = $_POST['evento_idCategoria'];
 $motivo = $_POST['evento_idMotivo'];
-$observacao = $_POST['evento_observacao'];
+$observacao = isset($_POST['evento_observacao']) && !empty(trim($_POST['evento_observacao'])) ? $_POST['evento_observacao'] : NULL;
 $data = $_POST['evento_data'];
 $hora = $_POST['evento_hora'];
-$cursista = $_POST['cursista_matricula'];
+$discente = $_POST['evento_idDiscente'];
 
 // Prepara e executa o INSERT
-$sql = "INSERT INTO tb_evento (evento_idColaborador, evento_idResponsavel, evento_idCategoria, evento_idMotivo, evento_observacao, evento_data, evento_hora, cursista_matricula)
-        VALUES ('$colaborador', '$responsavel', '$categoria', '$motivo', '$observacao', '$data', '$hora', '$cursista')";
+$sql = "INSERT INTO tb_sisco_evento (
+    evento_idColaborador, 
+    evento_idResponsavel, 
+    evento_idCategoria, 
+    evento_idMotivo, 
+    evento_observacao, 
+    evento_data, 
+    evento_hora, 
+    evento_idDiscente
+) VALUES (
+    '$colaborador', 
+    '$responsavel', 
+    '$categoria', 
+    '$motivo', 
+    " . ($observacao !== NULL ? "'$observacao'" : "NULL") . ", 
+    '$data', 
+    '$hora', 
+    '$discente'
+)";
+
 
 if ($conn->query($sql) === TRUE) {
     echo "Evento cadastrado com sucesso!";
-    header("Location: index.html"); // redireciona de volta
+    header("Location: ../cadastro-eventos/index.html");
+    exit;
 } else {
     echo "Erro: " . $sql . "<br>" . $conn->error;
 }
