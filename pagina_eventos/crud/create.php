ffab920bf8205251f5fe0ba66a5d37ec31c5ffa7
Andre-gusto <Andrelopes4808@gmail.com>
2025-06-03 22:27:56 -0300

    CRUD

--- pagina_eventos/crud/create.php
+++ pagina_eventos/crud/create.php
@@ -1,18 +1,19 @@
 <?php
 include 'db.php';
 
+$id = $_POST['evento_id'];
 $colaborador = $_POST['evento_idColaborador'];
 $responsavel = $_POST['evento_idResponsavel'];
-$categoria = $_POST['event_idCategoria'];
+$categoria = $_POST['evento_idCategoria'];
 $motivo = $_POST['evento_idMotivo'];
 $observacao = $_POST['evento_observacao'];
 $data = $_POST['evento_data'];
 $hora = $_POST['evento_hora'];
-$cursista = $_POST['cursista_matricula'];
+$discente = $_POST['evento_idDiscente'];
 
-$sql = "INSERT INTO tb_evento (evento_idColaborador, evento_idResponsavel, evento_idCategoria, evento_idMotivo, evento_observacao, evento_data, evento_hora, cursista_matricula)
-        VALUES ('$colaborador', '$responsavel', '$categoria', '$motivo', '$observacao', '$data', '$hora', '$cursista')";
+$sql = "INSERT INTO tb_sisco_evento (evento_idColaborador, evento_idResponsavel, evento_idCategoria, evento_idMotivo, evento_observacao, evento_data, evento_hora, evento_idDiscente)
+        VALUES ('$colaborador', '$responsavel', '$categoria', '$motivo', '$observacao', '$data', '$hora', '$discente')";
 
 $conn->query($sql);
-header("Location: index.php");
+header("Location: ../cadastro-eventos/index.html");
 ?>
