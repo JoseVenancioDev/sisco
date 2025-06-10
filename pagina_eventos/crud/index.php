cba6e529ba7fcc723873aaa79764d5e3ca10e649
Thiago-Ferreira-da-Silva <thiagoferreiradasilva408@gmail.com>
2025-06-10 13:36:53 -0300

    select of most data

--- pagina_eventos/crud/index.php
+++ pagina_eventos/crud/index.php
@@ -1,20 +1,26 @@
 <?php
 include 'db.php';
+header('Content-Type: application/json');
 
 // Consulta os dados
-$sql = "SELECT 
-    evento_id,
-    evento_data,
-    evento_hora,
-    evento_idResponsavel,
-    evento_idColaborador,
-    evento_idDiscente,
-    evento_idCursista,
-    evento_idCategoria,
-    evento_idMotivo,
-    evento_observacao,
-FROM tb_sisco_evento
-ORDER BY evento_data DESC, evento_hora DESC";
+$sql = "SELECT e.evento_id,
+       d.discente_nome AS nome_discente,
+       c.colaborador_nome AS nome_colaborador,
+       r.responsavelLegal_nome AS nome_responsavel,
+       e.evento_idCategoria,
+       e.evento_idMotivo AS motivo_nome,
+       e.evento_data,
+       e.evento_hora,
+       e.evento_observacao,
+       e.evento_dateTime
+FROM tb_sisco_evento e
+LEFT JOIN tb_jmf_discente d ON e.evento_idDiscente = d.discente_matricula
+LEFT JOIN tb_jmf_colaborador c ON e.evento_idColaborador = c.colaborador_matricula
+LEFT JOIN tb_jmf_responsavellegal r ON e.evento_idResponsavel = r.responsavelLegal_id
+LEFT JOIN tb_sisco_eventomotivo m ON e.evento_idMotivo = m.eventoMotivo_id
+
+ORDER BY e.evento_data DESC
+";
 
 $result = $conn->query($sql);
 
@@ -29,6 +35,11 @@ while ($row = $result->fetch_assoc()) {
 echo json_encode([
     "data" => $eventos
 ]);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 $conn->close();
 ?>
