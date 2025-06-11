9c2edcf05317c6ac8c3d1e0e92b8ace72bbc1a83
Thiago-Ferreira-da-Silva <thiagoferreiradasilva408@gmail.com>
2025-06-11 13:39:03 -0300

    Select da datatables finalizada(um erro em final de arquivo index.html em fetch()

--- pagina_eventos/crud/index.php
+++ pagina_eventos/crud/index.php
@@ -7,17 +7,23 @@ $sql = "SELECT e.evento_id,
        d.discente_nome AS nome_discente,
        c.colaborador_nome AS nome_colaborador,
        r.responsavelLegal_nome AS nome_responsavel,
-       e.evento_idCategoria,
-       e.evento_idMotivo AS motivo_nome,
+       ec.eventoCategoria_nome AS nome_categoria,
+       m.eventoMotivo_nome AS motivo_nome,
        e.evento_data,
        e.evento_hora,
        e.evento_observacao,
        e.evento_dateTime
 FROM tb_sisco_evento e
-LEFT JOIN tb_jmf_discente d ON e.evento_idDiscente = d.discente_matricula
-LEFT JOIN tb_jmf_colaborador c ON e.evento_idColaborador = c.colaborador_matricula
-LEFT JOIN tb_jmf_responsavellegal r ON e.evento_idResponsavel = r.responsavelLegal_id
-LEFT JOIN tb_sisco_eventomotivo m ON e.evento_idMotivo = m.eventoMotivo_id
+LEFT JOIN tb_jmf_discente d ON
+ e.evento_idDiscente = d.discente_matricula
+LEFT JOIN tb_jmf_colaborador c ON
+ e.evento_idColaborador = c.colaborador_matricula
+LEFT JOIN tb_jmf_responsavellegal r ON
+ e.evento_idResponsavel = r.responsavelLegal_id
+LEFT JOIN tb_sisco_eventomotivo m ON
+ e.evento_idMotivo = m.eventoMotivo_id
+LEFT JOIN tb_sisco_eventocategoria ec ON
+ e.evento_idCategoria = ec.eventoCategoria_id
 
 ORDER BY e.evento_data DESC
 ";
